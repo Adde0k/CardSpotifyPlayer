e826f5de67ef0c12399c48cb3540a766b0d47ed8
Adde0k <andkar0729@skola.goteborg.se>
2022-09-14 12:13:02 +0200

    Setup, added depencencies, created initial module, etc.
    Co-authored-by: Luca A. <lucale1114@users.noreply.github.com>

---
+++ src/app/card-spotify-player/card-spotify-player.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CardSpotifyPlayerComponent } from './card-spotify-player.component';
+
+describe('CardSpotifyPlayerComponent', () => {
+  let component: CardSpotifyPlayerComponent;
+  let fixture: ComponentFixture<CardSpotifyPlayerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [CardSpotifyPlayerComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CardSpotifyPlayerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/card-spotify-player/card-spotify-player.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-spotify-player',
+  templateUrl: './card-spotify-player.component.html',
+  styleUrls: ['./card-spotify-player.component.scss'],
+})
+export class CardSpotifyPlayerComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
